675e02813f6feaf04900ae71d2fb3808c425400c
Stuart Wheater <stuart.wheater@arjuna.com>
2021-12-13 11:56:52 +0000

    Updated 'id'

--- manifests/repository.pp
+++ manifests/repository.pp
@@ -25,9 +25,12 @@ class mongodb::repository {
         location    => 'http://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0',
         release     => 'multiverse',
         repos       => '',
-        key         => { 'server' => 'hkp://keyserver.ubuntu.com:80', 'id' =>  '42F3E95A2C4F08279C4960ADD68FA50FEA312927' },
+#        key         => { 'server' => 'http://repo.mongodb.org:80', 'id' => 'F5679A222C647C87527C2F8CB00A0BD1E2C63C11' },
+        key         => 'F5679A222C647C87527C2F8CB00A0BD1E2C63C11',
         include     => { 'src' => false },
         notify      => Class['apt::update'],
+#        allow_unsigned => false,
+        allow_unsigned => true,
       }
     }
     'Centos': {
